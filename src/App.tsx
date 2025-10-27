440c96c1731ad22c785f974648a3f78fe27a1f2f
Thomas Hodges <thodges@javascriptonline.org>
2025-10-26 22:05:13 -0700

    movable marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-// App.tsx
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+import { RMap, RMarker } from 'maplibre-react-components';
+import { useState } from 'react';
+
+import type { MapLayerMouseEvent } from 'maplibre-gl';
+const mountain: [number, number] = [6.4546, 46.1067];
+
 function App() {
+  const [markerPosition, setMarkerPosition] = useState<null | [number, number]>(
+    null
+  );
+
+  const handleClick = (e: MapLayerMouseEvent) =>
+    setMarkerPosition(e.lngLat.toArray());
   return (
-    <>
-      <h1>Hello Map</h1>
-    </>
+    <RMap
+      minZoom={6}
+      onClick={handleClick}
+      initialCenter={mountain}
+      initialZoom={8}
+      mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
+    >
+      <RMarker longitude={mountain[0]} latitude={mountain[1]} />
+      {markerPosition && (
+        <RMarker
+          longitude={markerPosition[0]}
+          latitude={markerPosition[1]}
+          // draggable
+          // onDragEnd={(e) => setMarkerPosition(e.lngLat.toArray())}
+        />
+      )}
+    </RMap>
   );
 }
 
